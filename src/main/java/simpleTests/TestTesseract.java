165fe93ab395b44c1882df9800e1a5a511a4ad38
Xingyu Yan <yxypriv@gmail.com>
2014-10-13 15:06:54 -0400

    init

---
+++ src/main/java/simpleTests/TestTesseract.java
@@ -0,0 +1,27 @@
+package simpleTests;
+
+import java.io.File;
+import java.net.URL;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+public class TestTesseract {
+	public static void main(String[] args) {
+		URL resource = TestTesseract.class.getClassLoader()//
+//				.getResource("images.jpg");
+//				.getResource("Test2-th.png");
+				.getResource("Test1.png");
+
+		File f = new File(resource.getFile());
+
+		try {
+			Tesseract instance = Tesseract.getInstance();
+//			Tesseract1 instance = new Tesseract1();
+			String doOCR = instance.doOCR(f);
+			System.out.println(doOCR);
+		} catch (TesseractException e) {
+			e.printStackTrace();
+		}
+	}
+}
