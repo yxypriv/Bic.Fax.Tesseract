31d5bd15e7c85d37ec0ad88a0cee1169b968826b
Xingyu Yan <yxypriv@gmail.com>
2014-10-17 15:58:21 -0400

    integrate carol's code

--- src/main/java/simpleTests/DetailedTest.java
+++ src/main/java/simpleTests/DetailedTest.java
@@ -1,8 +1,10 @@
 package simpleTests;
 
+import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.nio.ByteBuffer;
 import java.util.List;
 
 import javax.imageio.IIOImage;
@@ -10,32 +12,48 @@ import javax.imageio.IIOImage;
 import net.sourceforge.tess4j.TessAPI;
 import net.sourceforge.tess4j.TessAPI.TessBaseAPI;
 import net.sourceforge.tess4j.TessAPI.TessOcrEngineMode;
-import net.sourceforge.tess4j.TessAPI.TessPageSegMode;
+import net.sourceforge.vietocr.ImageHelper;
 import net.sourceforge.vietocr.ImageIOHelper;
 
+import com.sun.jna.Pointer;
+
 public class DetailedTest {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		TessAPI api = TessAPI.INSTANCE;
 		TessBaseAPI handle = api.TessBaseAPICreate();
+		ImageHelper ih = new ImageHelper();
+		ImageIOHelper iio = new ImageIOHelper();
 		api.TessBaseAPIInit2(handle, "./", "eng", TessOcrEngineMode.OEM_TESSERACT_CUBE_COMBINED);
-		api.TessBaseAPISetPageSegMode(handle, TessPageSegMode.PSM_AUTO);
-		
+//		api.TessBaseAPISetPageSegMode(handle, TessPageSegMode.PSM_AUTO);
 		
 		URL resource = TestTesseract.class.getClassLoader()//
-//				.getResource("images.jpg");
-				.getResource("Test2.png");
+				.getResource("images.jpg");
+//				.getResource("character/1.png");
 //				.getResource("Test5.png");
 
 		File f = new File(resource.getFile());
-		
-		try {
-			List<IIOImage> iioImageList = ImageIOHelper.getIIOImageList(f);
-			
-		
-		} catch (IOException e) {
-			e.printStackTrace();
+
+		List<IIOImage> imageList = ImageIOHelper.getIIOImageList(f);
+		RenderedImage rimage = null;
+		for (IIOImage oimage : imageList) {
+			rimage = oimage.getRenderedImage();
 		}
-		
-		
+		int width = rimage.getWidth();
+		int height = rimage.getHeight();
+		int bpp = rimage.getColorModel().getPixelSize();
+		int bytespp = bpp / 8;
+		int bytespl = (int) Math.ceil(width * bpp / 8.0);
+		ByteBuffer bb2 =  ImageIOHelper.getImageByteBuffer(rimage);
+//		BufferedImage bi = ImageIO.read(f);
+//		BufferedImage bi2 = new BufferedImage(bi.getWidth(),
+//				bi.getHeight(), BufferedImage.TYPE_INT_ARGB);
+//		BufferedImage binary = ih.convertImage2Binary(bi2);
+//		ByteBuffer bb = iio.convertImageData(binary);
+	 	
+	 	api.TessBaseAPISetImage(handle, bb2, width, height, bytespp, bytespl);
+	 	Pointer p = api.TessBaseAPIGetUTF8Text(handle);
+	 	
+	 	String str = p.getString(0);
+	 	System.out.println(str);
 	}
 }
--- src/main/java/simpleTests/TestTesseract.java
+++ src/main/java/simpleTests/TestTesseract.java
@@ -1,21 +1,16 @@
 package simpleTests;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
-import java.util.List;
-
-import javax.imageio.IIOImage;
 
 import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
-import net.sourceforge.vietocr.ImageIOHelper;
 
 public class TestTesseract {
 	public static void main(String[] args) {
 		URL resource = TestTesseract.class.getClassLoader()//
 //				.getResource("images.jpg");
-				.getResource("bluredimage.jpg");
+				.getResource("PoorLight&blurness.jpg");
 //				.getResource("Test5.png");
 
 		File f = new File(resource.getFile());
